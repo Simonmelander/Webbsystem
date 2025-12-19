ad0ee240b30732fcb8abdef5e70e5dbb36ef3cb7
Edvard Sterky <eddes12@gmail.com>
2025-12-19 14:47:47 +0100

    databas setup

---
+++ WebApplication1/WebApplication1/Models/Data/MyAppContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models.Data
+{
+    public class MyAppContext : DbContext
+    {
+        public MyAppContext(DbContextOptions<MyAppContext>options) : base(options)
+        { 
+        }
+    }
+}
--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<MyAppContext>(options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
