b0783793a5c3198665fee0d0c65e23177f07d1d9
jofra2003 <joel.fransson03@gmail.com>
2025-12-22 12:22:59 +0100

    Add initial migration for DbSets and update context
    Added migration files for creating Projects, Users, Cvs, Messages, Educations, Experiences, and Skills tables with appropriate relationships. Updated MyAppContext to configure Message Sender and Receiver relations with DeleteBehavior.Restrict to prevent multiple cascade paths.

--- WebApplication1/CvProject.View/Migrations/20251222112226_DbSets.Designer.cs
+++ WebApplication1/CvProject.View/Migrations/20251222112226_DbSets.Designer.cs
@@ -4,6 +4,7 @@ using CvProject.View.Models.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CvProject.Migrations
 {
     [DbContext(typeof(MyAppContext))]
-    partial class MyAppContextModelSnapshot : ModelSnapshot
+    [Migration("20251222112226_DbSets")]
+    partial class DbSets
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- WebApplication1/CvProject.View/Models/Data/MyAppContext.cs
+++ WebApplication1/CvProject.View/Models/Data/MyAppContext.cs
@@ -8,6 +8,25 @@ namespace CvProject.View.Models.Data
         public MyAppContext(DbContextOptions<MyAppContext>options) : base(options)
         { 
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Konfigurera Sender-relationen
+            modelBuilder.Entity<Message>()
+                .HasOne(m => m.Sender)
+                .WithMany() // Eller .WithMany(u => u.SentMessages) om du har en lista i User
+                .HasForeignKey(m => m.SenderId)
+                .OnDelete(DeleteBehavior.Restrict); // Detta stoppar "Multiple Cascade Paths"
+
+            // Konfigurera Receiver-relationen
+            modelBuilder.Entity<Message>()
+                .HasOne(m => m.Receiver)
+                .WithMany() // Eller .WithMany(u => u.ReceivedMessages)
+                .HasForeignKey(m => m.ReceiverId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
         public DbSet<User> Users { get; set; } = default!;
         public DbSet<Cv> Cvs { get; set; } = default!;
         public DbSet<Project> Projects { get; set; } = default!;
