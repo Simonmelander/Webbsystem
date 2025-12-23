5c2755316dcdea004753de72e814eba135c37205
jofra2003 <joel.fransson03@gmail.com>
2025-12-23 12:14:39 +0100

    Add CV creation feature with controller and views
    Introduces CvController, CvCreateViewModel, and views for creating and listing CVs. Updates the layout to include a navigation link to the CV creation page. This enables users to input education, experience, and skills when creating a new CV.

---
+++ WebApplication1/CvProject.View/Models/CvCreateViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CvProject.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace CvProject.View.Models
+{
+    public class CvCreateViewModel
+    {
+        public List<Education> Educations { get; set; } = new List<Education>();
+        public List<Experience> Experiences { get; set; } = new List<Experience>();
+        public List<Skill> Skills { get; set; } = new List<Skill>();
+    }
+}
