9260f496c8ccfe9668b75c210138244c38ff0878
Simonmelander <simon.melander02@gmail.com>
2025-12-23 13:46:47 +0100

    123
    123

--- WebApplication1/CvProject.View/Program.cs
+++ WebApplication1/CvProject.View/Program.cs
@@ -13,21 +13,21 @@ builder.Services.AddDbContext<MyAppContext>(options =>
     options.UseSqlServer(connectionString));
 
 // 2. Koppla Identity (Inloggningssystemet)
-// Här kopplar vi din specifika User-klass och din DbContext
+
 builder.Services.AddIdentity<User, IdentityRole>(options =>
 {
-    // Konfigurera lösenordskrav (gör det enkelt för testning)
+    
     options.SignIn.RequireConfirmedAccount = false;
     options.Password.RequireDigit = false;
     options.Password.RequireNonAlphanumeric = false;
     options.Password.RequireUppercase = false;
     options.Password.RequiredLength = 3;
-    options.User.RequireUniqueEmail = true; // Kräv unik e-post
+    options.User.RequireUniqueEmail = true;
 })
 .AddEntityFrameworkStores<MyAppContext>()
 .AddDefaultTokenProviders();
 
-// 3. Konfigurera Cookie-inställningar (Vart oinloggade skickas)
+
 builder.Services.ConfigureApplicationCookie(options =>
 {
     options.LoginPath = "/Account/Login";
@@ -36,11 +36,11 @@ builder.Services.ConfigureApplicationCookie(options =>
 });
 
 builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages(); // Bra att ha kvar om du använder Identity scaffolding i framtiden
+builder.Services.AddRazorPages(); 
 
 var app = builder.Build();
 
-// --- Konfigurera HTTP-pipelinen ---
+
 
 if (!app.Environment.IsDevelopment())
 {
@@ -53,9 +53,9 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-// Ordningen här är viktig!
-app.UseAuthentication(); // 1. Vem är du?
-app.UseAuthorization();  // 2. Vad får du göra?
+
+app.UseAuthentication(); 
+app.UseAuthorization();  
 
 app.MapControllerRoute(
     name: "default",
