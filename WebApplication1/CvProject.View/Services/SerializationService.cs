1e5d5aa3a45734a88da8d9299a2688c470a97173
jofra2003 <joel.fransson03@gmail.com>
2026-01-12 12:59:15 +0100

    Export to XML
    Functionality to export to XML, using serialization and DTO.

---
+++ WebApplication1/CvProject.View/Services/SerializationService.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml.Serialization;
+
+namespace CvProject.View.Services
+{
+    public static class SerializationService
+    {
+        public static string SerializeToXML<T>(T obj)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(T));
+
+            using(var stringWriter = new StringWriter())
+            {
+                xmlSerializer.Serialize(stringWriter, obj);
+                return stringWriter.ToString();
+            }
+        }
+    }
+}
